f84e6933000872bfcae697f2c7a3ae88fb83d1d9
jessanderson222 <jessanderson222@gmail.com>
2018-10-31 09:43:49 -0400

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,24 @@
+class Person
+  def initialize(person_name)
+  @name = person_name
+end
+
+def name(person_name)
+  @name
+end
+end 
+
+
+class Dog
+
+  def initialize(dog_name, breed="Mutt")
+    @name = dog_name
+    @breed = breed
+  end
+
+  def name(dog_name)
+   @name
+ end
+  #...
+
+end
